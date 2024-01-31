c170a570cba64758b1a19394d5cea787fbf4786a
Christinat Jonathan <Jonathan.Christinat@swisscom.com>
2024-01-31 14:20:15 +0100

    changed the fetch to the website for test

--- GPT_Frontend/src/components/Chat.jsx
+++ GPT_Frontend/src/components/Chat.jsx
@@ -21,7 +21,8 @@ const Chat = () => {
       };
     
       try {
-        const response = await fetch("https://gpt-v2-6y9v.vercel.app/", options);
+        //"https://gpt-v2-6y9v.vercel.app/"
+        const response = await fetch("http://localhost:8000/aichatbot", options);
         const data = await response.json();
         setMessage(data.choices[0].message)
       } catch (error) {
